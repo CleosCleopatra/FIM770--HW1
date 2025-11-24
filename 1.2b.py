3939a18d8d03cdf639da04e5d0cb4c0ac56b4465
CleosCleopatra <elsaerk09@gmail.com>
2025-11-24 13:07:33 +0100

    Done?

--- 1.2b.py
+++ 1.2b.py
@@ -3,28 +3,28 @@ import numpy as np
 
 rs = np.linspace(-4,4, 400)
 hs = np.linspace(-4,4,400)
+R, H = np.meshgrid(rs,hs)
+D = R**2 + 4*H
 
-def calc(r,h):
-    sqr = np.sqrt(r**2 + 4*h)
-    c11 = r + sqr
-    c12 = r - sqr
-    return c11/2, c12/2
+pos = D >= 0
 
-r_list, h_list = [], []
-x_list=[]
-for r in rs:
-    for h in hs:
-        x1, x2 = calc(r,h)
-        x_list.append(x1)
-        x_list.append(x2)
-        r_list.append(r)
-        r_list.append(r)
-        h_list.append(h)
-        h_list.append(h)
+#fixed points
+x_plus = np.where(pos, (R + np.sqrt(D))/2, np.nan)
+x_minus = np.where(pos, (R - np.sqrt(D))/2, np.nan)
+
+
 
 
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.scatter(h_list, r_list, x_list)
+#cs = ax.contourf(R,H,Z, levels = [-0.5, 0.5, 1.5, 2.5])
+ax.plot_surface(R, H, x_plus, color='red')
+ax.plot_surface(R, H, x_minus)
+
+ax.set_xlabel("r")
+ax.set_ylabel("h")
+ax.set_zlabel("x*")
+ax.set_title("Fixed point surface")
+
 plt.show()
